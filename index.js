614305050b36fc5281e879bf8162d226405a543c
Parv Sharma <sharmaparv.2004@gmail.com>
2024-02-16 11:49:26 +0530

    changes

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonfile = require('jsonfile')
+const moment = require('moment')
+const simpleGit = require('simple-git')
+const path = './data.json'
+
+const date = moment().format()
+
+const data = {
+    DATE: date
+}
+
+jsonfile.writeFile(path, data)
+
+simpleGit().add([path]).commit(date, {'--date': date}).push()
